82ce62915c120404b15b61595cafa1d6a3bfbf8b
Marcus <44581072+marcusdallum@users.noreply.github.com>
2022-03-07 16:02:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 @app.route("/")
 def index():
-  return 'Index'
+  return render_template('about.html')
 @app.route("/blog")
 def blog():
   return "Blog"
