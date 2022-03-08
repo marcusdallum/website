e189311056c0b897a6f5bfe00722dc4a29a9e1ab
Marcus <44581072+marcusdallum@users.noreply.github.com>
2022-03-07 22:15:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import json
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+
+f = open("static/website.json", 'a')
+json_dict = json.load(f)
+f.close()
+
+@app.route("/test")
+def test():
+  return(json_dict)
+
 @app.route("/")
 def index():
   return render_template('about.html')
+
 @app.route("/blog")
 def blog():
   return "Blog"
