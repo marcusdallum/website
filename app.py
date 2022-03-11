3efda98f38966d727892cf27aa543e501add6aa6
Marcus <44581072+marcusdallum@users.noreply.github.com>
2022-03-11 13:41:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 
 DATABASE = 'test.db'
 
+titles = ['hello_world.html']
 
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
+
 
 
 @app.teardown_appcontext
@@ -19,12 +21,15 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-@app.route("/posts/hello_world.html")
-def test():
+@app.route("/posts/<title>")
+def posts(title):
   conn = get_db_connection()
   post = conn.execute('select * from posts where indexNum=1').fetchall()
   conn.close()
-  return render_template('hello_world.html' , post=post)
+  url = None
+  if title in titles:
+        url = titles[title]
+  return render_template('hello_world.html' , post=post title=title)
 
 @app.route("/")
 def index(title=None):
