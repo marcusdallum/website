4afe8b7f88748099a099fa07c2446bc9c7f6b086
Marcus <44581072+marcusdallum@users.noreply.github.com>
2022-04-05 17:25:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,6 @@ def feed():
     while x < 5:
         txt = (slash.entries[x].summary_detail['value'])
         y = re.split("<p", txt)
-        feeds_detail_list.append(y[0])
         feeds_dict[x] = {'title':slash.entries[x].title,'data':y[0]}
         x=x+1
     return render_template('feed.html', feeds_dict=feeds_dict)
