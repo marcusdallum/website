3a8ad668e80ea34f919d7ee58dfc728b0a28c272
Marcus <44581072+marcusdallum@users.noreply.github.com>
2024-10-27 21:59:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,57 +25,17 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-@app.route("/blog1")
+@app.route("/blog")
 def posts():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('posts.html', posts=posts ,  title="The IT Manager")
-
-#@app.route("/posts/<slug>")
-#def posts(slug):
-#  post = []
-#  conn = get_db_connection()
-#  posts = conn.execute("select * from posts").fetchall()
-#  conn.close()
-#  posts_dict = {}
-#  for x in posts:
-#    if x[1] == slug:
-#      title = x['title']
-#      #data = markdown.markdown(x['data'])
-#      #slug = x['slug']
-#      #date = x['date']
-#      #posts_dict[slug] = {'title':title,'data':data,'slug':slug,'date':date}
-#      return render_template('hello_world.html' , posts_dict=posts_dict , slug=slug)
-  
-#  return render_template('about.html', title="The IT Manager")
-  
-  
-
+    
 @app.route("/")
 def index(title=None):
   return render_template('about.html', title="The IT Manager")
-
-@app.route("/blog")
-def blog():
-  titles = []
-  conn = get_db_connection()
-  posts = conn.execute('select * from posts ORDER BY date DESC').fetchall()
-  db_title = conn.execute('select title from posts ORDER BY date DESC').fetchall()
-  conn.close()
-  posts_dict = {}
-
-  for x in posts:
-    title = x['title']
-    data =  markdown.markdown(x['data'])
-    slug = x['slug']
-    date = x['date']
-    posts_dict[slug] = {'title':title,'data':data,'slug':slug,'date':date}
     
-
-                                       
-  return render_template('blog.html' , posts_dict=posts_dict)
-
 @app.route("/links")
 def links():
   return render_template('links.html')
